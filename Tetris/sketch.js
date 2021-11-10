eec159ff639b2c2e0d5f3a503816bcbb5d768162
Sreyeso <88210380+Sreyeso@users.noreply.github.com>
2021-11-10 12:23:02 -0500

    Cambios - Dibujo y teclado
    Entrada y movimiento limitado(falta) de las piezas por teclado, y cambios en la manera de representar el tablero de juego.

--- Tetris/sketch.js
+++ Tetris/sketch.js
@@ -1,45 +1,20 @@
 
 //Variables globales
 let filas=10;
-let columnas=20;
+let columnas=17;
+let colores = [];
 let tablero = [];
 
-function setup() {
-  createCanvas(300,600);
-  //Inicializar tablero - blanco
-  for (var i=0;i<filas;i++){
-    tablero[i]=[];
-    for (var j=0;j<columnas;j++){
-      tablero[i].push(255);
-    }
-  }
-}
-
-function draw() {
-  background(255,255,210);
-
-  //Imprimir tablero
-  for (var i=0;i<filas;i++){
-    for (var j=0;j<columnas;j++){
-      var x = i*30;
-      var y = j*30;
-      stroke(100);
-      fill(tablero[i][j]);
-      rect(x,y,30,30);
-    }
-  }
-  
-  p1 = new pieza(1,1,1);
-  p1.dibujar();
-  
-}
-
+//Clase
 class pieza{
-  constructor(y,x,ref){
+
+  constructor(y,x,ref,estado,vel){
 
     this.x=x;
     this.y=y;
     this.rot=0;
+    this.velocidad=vel;
+    this.estado=estado; // 1-Queue 2-inplay 3-Out of play 
 
     //Nombres de las piezas - O I J L S T Z
     //Equivalente en números  1 2 3 4 5 6 7
@@ -50,7 +25,6 @@ class pieza{
       [ [1,1],
         [1,1]]; 
     }
-
     if (ref==2){
       this.f=4;this.c=4;this.color=color(51, 204, 255); 
       this.forma=
@@ -59,7 +33,6 @@ class pieza{
         [0,0,0,0],
         [0,0,0,0]];
     }
-
     if (ref==3){
       this.f=3;this.c=3;this.color=color(0, 0, 204);
       this.forma=
@@ -67,7 +40,6 @@ class pieza{
         [1,1,1],
         [0,0,0]];
     }
-
     if (ref==4){
       this.f=3;this.c=3;this.color=color(255, 153, 51); 
       this.forma=
@@ -75,7 +47,6 @@ class pieza{
         [1,1,1],
         [0,0,0]];  
     }
-
     if (ref==5){
       this.f=3;this.c=3;this.color=color(0, 204, 0);
       this.forma=
@@ -83,7 +54,6 @@ class pieza{
         [1,1,0],
         [0,0,0]];
     }
-
     if (ref==6){
       this.f=3;this.c=3;this.color=color(204, 0, 204);
       this.forma=
@@ -91,7 +61,6 @@ class pieza{
         [1,1,1],
         [0,0,0]];
     }
-
     if (ref==7){
       this.f=3;this.c=3;this.color=color(255, 0, 0);
       this.forma=
@@ -99,30 +68,87 @@ class pieza{
         [0,1,1],
         [0,0,0]];
     }
-  
+
   }
 
-
   dibujar(){
-    for (var i=0;i<this.f;i++){
-      for (var j=0;j<this.c;j++){
+    for (let i=0;i<this.f;i++){
+      for (let j=0;j<this.c;j++){
         if (this.forma[i][j]==1){
-          tablero[j+this.x][i+this.y]=this.color;
+          colores[j+this.x][i+int(this.y)]=this.color;
         }
       }
     }
   }
 
-  rotar(){
-
+  bajar(){
+    if (this.y<columnas-this.c){this.y=this.y+this.velocidad;}
+    if (keyIsDown(DOWN_ARROW)){
+      if(this.y<columnas-this.c){this.y=this.y+1;}
+    }
   }
 
   moverderizq(){
-
+    if (keyIsDown(LEFT_ARROW)){
+      if (this.x>0){this.x-=1;}
+    }
+    if (keyIsDown(RIGHT_ARROW)){
+      if (this.x<filas-this.f){this.x+=1;}
+    } 
+  }
+  rotar(){
   }
 
-  bajar(){
+}
 
+//Funciones
+
+//Setup
+function setup() {
+  //Inicializar ventana
+  createCanvas(300,510);
+  //Crear tablero a imprimir
+  for (let i=0;i<filas;i++){
+    colores[i]=[];
+    for (let j=0;j<columnas;j++){
+      colores[i].push(255);
+    }
+  }
+  //Crear tablero de referencia
+  for (let i=0;i<filas;i++){
+    tablero[i]=[];
+    for (let j=0;j<columnas;j++){
+      tablero[i].push(255);
+    }
+  }
+  ///Inicializar objetos
+  //X,Y,IDPIEZA,ESTADO,VELOCIDAD
+  p1 = new pieza(1,1,3,2,0.05);
+}
+
+//Main
+function draw() {
+
+  //Resetear el tablero de las piezas que aún no son fijas
+  for (let i=0;i<filas;i++){
+    for (let j=0;j<columnas;j++){
+      colores[i][j]=tablero[i][j];
+    }
   }
 
+  //Dibujar las piezas activas
+  p1.dibujar();
+  p1.bajar();
+  p1.moverderizq();
+
+  //Imprimir tablero
+  for (let i=0;i<filas;i++){
+    for (let j=0;j<columnas;j++){
+      let x = i*30;
+      let y = j*30;
+      stroke(100);
+      fill(colores[i][j]);
+      rect(x,y,30,30);
+    }
+  } 
 }
